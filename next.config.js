33a642ce5965061e15e85ed25b7b1782c4055125
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-18 10:48:13 +0700

    Change how to define next.config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@ const withTypeScript = require('@zeit/next-typescript');
 const withOffline = require('next-offline')
 
 
-module.exports = (_, { defaultConfig }) => {
-  const config = {
-    ...defaultConfig,
-    target: 'serverless',
-  };
-  return withOffline(withTypeScript(config));
-};
+// module.exports = (_, { defaultConfig }) => {
+//   const config = {
+//     ...defaultConfig,
+//     target: 'serverless',
+//   };
+//   return withOffline(withTypeScript(config));
+// };
+
+module.exports = withOffline(withTypeScript({
+  target: 'serverless', 
+}));
