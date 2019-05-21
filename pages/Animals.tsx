b578416c4a7e06080199c7661246f88dabd70327
queq1890 <32263803+queq1890@users.noreply.github.com>
2019-05-21 17:54:09 +0700

    Tempo

---
+++ pages/Animals.tsx
@@ -0,0 +1,9 @@
+import Footer from '../presentational/Footer';
+
+const Animals = () => (
+  <div>
+    <Footer>footer is here</Footer>
+  </div>
+);
+
+export default Animals;
---
+++ presentational/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Footer = styled.div`
+  font-size: 16px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 20px;
+  height: 20px;
+  background-color: #fff;
+  -webkit-box-shadow: 0px -1px 10px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px -1px 10px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px -1px 10px 0px rgba(0, 0, 0, 0.75);
+`;
+
+export default Footer;
